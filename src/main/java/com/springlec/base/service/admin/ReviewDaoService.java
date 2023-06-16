1e0e2023d62c17d790d80f15d2726f2e0fc52676
Oh-Kang94 <okh19941994@naver.com>
2023-06-16 21:44:27 +0900

    move CSS,JS,vendor,lib
    commit #007

---
+++ src/main/java/com/springlec/base/service/admin/ReviewDaoService.java
@@ -0,0 +1,19 @@
+package com.springlec.base.service.admin;
+
+import java.util.List;
+
+import com.springlec.base.model.admin.ReviewDto;
+
+public interface ReviewDaoService {
+	public List<ReviewDto> searchBoard() throws Exception;
+
+	public int insertBoard_01(int seq, String parent, String layer, String pcode, String adminid) throws Exception;
+
+	public int insertBoard_02(int seq, String adminid, String context) throws Exception;
+
+	public ReviewDto searchBoardseq() throws Exception;
+
+	public int modifyBoard(int seq, String adminid, String context) throws Exception;
+
+	public int deleteBoard(int seq) throws Exception;
+}
