eb449d3a9b50e8db6079e33fd5f08076a5037e42
Oh-Kang94 <okh19941994@naver.com>
2023-06-17 02:33:22 +0900

    add productDao
    commit #011

---
+++ src/main/java/com/springlec/base/dao/admin/ProductDao.java
@@ -0,0 +1,22 @@
+package com.springlec.base.dao.admin;
+
+import java.util.ArrayList;
+
+import com.springlec.base.model.admin.ProductDto;
+
+public interface ProductDao {
+	public ArrayList<ProductDto> searchOrders() throws Exception;
+
+	public ArrayList<ProductDto> searchProduct() throws Exception;
+
+	public ArrayList<ProductDto> findProduct(String pcode) throws Exception;
+	
+	public int insertProduct(String name, String category, String rice, String cook1, String cook2, String cook3,
+			String soup, String photo, int calories) throws Exception;
+	
+	public int updateProduct(String pcode, String name, String category, String rice, String cook1, String cook2,
+			String cook3, String soup, String photo, int calories) throws Exception;
+
+	public ProductDto getPname(String pcode) throws Exception;
+
+}
---
+++ src/main/java/com/springlec/base/dao/admin/ProductDaoImpl.java
@@ -0,0 +1,60 @@
+package com.springlec.base.dao.admin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.springlec.base.model.admin.ProductDto;
+
+public class ProductDaoImpl implements ProductDao {
+	
+	SqlSession sqlsession;
+	
+	public static String namespace = "com.spring.base.dao.admin.ProductDao";
+	
+	@Override
+	public ArrayList<ProductDto> searchOrders() throws Exception {
+		// TODO Auto-generated method stub
+		List<ProductDto> resultList = sqlsession.selectList(namespace + ".searchOrders");
+		ArrayList<ProductDto> productList =new ArrayList<>(resultList);
+		return productList;
+	}
+
+	@Override
+	public ArrayList<ProductDto> searchProduct() throws Exception {
+		// TODO Auto-generated method stub
+		List<ProductDto> resultList = sqlsession.selectList(namespace + ".searchProduct");
+		ArrayList<ProductDto> productList =new ArrayList<>(resultList);
+		return productList;
+	}
+
+	@Override
+	public ArrayList<ProductDto> findProduct(String pcode) throws Exception {
+		// TODO Auto-generated method stub
+		List<ProductDto> resultList = sqlsession.selectList(namespace + ".findProduct");
+		ArrayList<ProductDto> productList =new ArrayList<>(resultList);
+		return productList;
+	}
+
+	@Override
+	public int insertProduct(String name, String category, String rice, String cook1, String cook2, String cook3,
+			String soup, String photo, int calories) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlsession.insert(namespace + ".insertProduct");
+	}
+
+	@Override
+	public int updateProduct(String pcode, String name, String category, String rice, String cook1, String cook2,
+			String cook3, String soup, String photo, int calories) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlsession.update(namespace + ".insertProduct");
+	}
+
+	@Override
+	public ProductDto getPname(String pcode) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlsession.selectOne(namespace+".getPname");
+	}
+
+}
