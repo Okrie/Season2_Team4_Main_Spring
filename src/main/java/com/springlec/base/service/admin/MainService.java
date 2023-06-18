e551e7bc9ed5a195a8fe09aef9a43958be05332e
Oh-Kang94 <okh19941994@naver.com>
2023-06-18 22:12:48 +0900

    add MainService
    commit #034

---
+++ src/main/java/com/springlec/base/service/admin/MainService.java
@@ -0,0 +1,22 @@
+package com.springlec.base.service.admin;
+
+import java.util.ArrayList;
+
+import com.springlec.base.model.admin.OrdersDto;
+import com.springlec.base.model.admin.SubscribeDto;
+
+public interface MainService {
+
+	public OrdersDto searchmonthSales() throws Exception;
+
+	public OrdersDto searchyearSalessolo() throws Exception;
+
+	public OrdersDto searchordersHowmany() throws Exception;
+
+	public OrdersDto searchtodo() throws Exception;
+	
+	public ArrayList<ArrayList<String>> searchyearsales() throws Exception;
+	
+	public SubscribeDto searchsubscribeSalessolo() throws Exception;
+	
+}
---
+++ src/main/java/com/springlec/base/service/admin/MainServiceImpl.java
@@ -0,0 +1,71 @@
+package com.springlec.base.service.admin;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springlec.base.dao.admin.OrdersDao;
+import com.springlec.base.dao.admin.SubscribeDao;
+import com.springlec.base.model.admin.OrdersDto;
+import com.springlec.base.model.admin.SubscribeDto;
+
+@Service
+public class MainServiceImpl implements MainService {
+	
+	@Autowired
+	SubscribeDao subscribedao;
+	
+	@Autowired
+	OrdersDao ordersdao;
+
+	@Override
+	public OrdersDto searchmonthSales() throws Exception {
+		// TODO Auto-generated method stub
+		return ordersdao.searchmonthSales();
+	}
+
+	@Override
+	public OrdersDto searchyearSalessolo() throws Exception {
+		// TODO Auto-generated method stub
+		return ordersdao.searchyearSalessolo();
+	}
+
+	@Override
+	public OrdersDto searchordersHowmany() throws Exception {
+		// TODO Auto-generated method stub
+		return ordersdao.searchordersHowmany();
+	}
+
+	@Override
+	public OrdersDto searchtodo() throws Exception {
+		// TODO Auto-generated method stub
+		return ordersdao.searchtodo();
+	}
+
+	@Override
+	public ArrayList<ArrayList<String>> searchyearsales() throws Exception {
+		ArrayList<OrdersDto> dtoOrders = ordersdao.searchyearsales();
+
+		ArrayList<ArrayList<String>> dataSales = new ArrayList<ArrayList<String>>();
+		ArrayList<String> headerRow = new ArrayList<String>();
+		headerRow.add("'월'");
+		headerRow.add("'판매액(₩)'");
+		dataSales.add(headerRow);
+		for (int i = 0; i < dtoOrders.size(); i++) {
+			OrdersDto ordersDto = dtoOrders.get(i);
+			ArrayList<String> row = new ArrayList<String>();
+			row.add("'" + ordersDto.getMonth() + "'");
+			row.add(Integer.toString(ordersDto.getTotalsales()));
+			dataSales.add(row);
+		}
+		return dataSales;
+	}
+
+	@Override
+	public SubscribeDto searchsubscribeSalessolo() throws Exception {
+		// TODO Auto-generated method stub
+		return subscribedao.searchsubscribeSalessolo();
+	}
+
+}
