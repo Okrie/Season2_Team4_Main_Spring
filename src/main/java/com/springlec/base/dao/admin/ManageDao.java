42bcf34812c13ce4cb744d37c740736b50a99782
Oh-Kang94 <okh19941994@naver.com>
2023-06-18 23:01:56 +0900

    fix ManageDao
    commit #043

--- src/main/java/com/springlec/base/dao/admin/ManageDao.java
+++ src/main/java/com/springlec/base/dao/admin/ManageDao.java
@@ -6,26 +6,26 @@ import com.springlec.base.model.admin.ManageDto;
 
 public interface ManageDao {
 	// 오더관련 재고 불러오기 : admin_searchOrders.do
-    public ManageDto searchOrders();
+    public ManageDto searchOrdersManage();
 
     // 오더관련 모든 정보 불러오기 : admin_searchOrders.do
-    public ManageDto searchUpdate(int ordercode);
+    public ManageDto searchUpdateManage(int ordercode);
 
     // 테이블 제품 관련 정보 불러오기 : admin_searchProduct.do
-    public ArrayList<ManageDto> searchProduct();
+    public ArrayList<ManageDto> searchProductManage();
 
     // 업데이트 관련 상품 관련 정보 불러오기 : admin_findproduct.do
-    public ArrayList<ManageDto> findProduct(String pcode);
+    public ArrayList<ManageDto> findProductManage(String pcode);
 
     // 데이터 입력하기 : admin_insertproduct.do
-    public int insertProduct(String adminid, int stock, int price);
+    public int insertProductManage(String adminid, int stock, int price);
 
     // 데이터 수정하기 : admin_updateproduct.do
-    public int updateProduct(String adminid, String pcode, int stock, int price);
+    public int updateProductManage(String adminid, String pcode, int stock, int price);
 
     // 상품 관련 정보 삭제하기 : admin_deleteproduct.do
-    public int deleteProduct(String pcode);
+    public int deleteProductManage(String pcode);
 
     // 상품 관련 정보 복구하기 : admin_deleteproduct.do
-    public int recoverProduct(String pcode);
+    public int recoverProductManage(String pcode);
 }
--- src/main/java/com/springlec/base/dao/admin/ManageDaoImpl.java
+++ src/main/java/com/springlec/base/dao/admin/ManageDaoImpl.java
@@ -10,52 +10,52 @@ import com.springlec.base.model.admin.ManageDto;
 public class ManageDaoImpl implements ManageDao {
 
 	SqlSession sqlSession;
-	
+
 	public static String namespace = "com.springlec.base.dao.admin.ManageDao";
-	
+
 	@Override
-    public ManageDto searchOrders() {
-        return sqlSession.selectOne("searchOrders");
-    }
+	public ManageDto searchOrdersManage() {
+		return sqlSession.selectOne(namespace + ".searchOrdersManage");
+	}
 
-    @Override
-    public ManageDto searchUpdate(int ordercode) {
-        return sqlSession.selectOne("searchUpdate", ordercode);
-    }
+	@Override
+	public ManageDto searchUpdateManage(int ordercode) {
+		return sqlSession.selectOne(namespace + ".searchUpdateManage", ordercode);
+	}
 
-    @Override
-    public ArrayList<ManageDto> searchProduct() {
-    	List<ManageDto> resultList = sqlSession.selectList(namespace + ".searchProduct");
-		ArrayList<ManageDto> manageList =new ArrayList<>(resultList);
+	@Override
+	public ArrayList<ManageDto> searchProductManage() {
+		List<ManageDto> resultList = sqlSession.selectList(namespace + ".searchProductManage");
+		ArrayList<ManageDto> manageList = new ArrayList<>(resultList);
 		return manageList;
-    	
-    }
 
-    @Override
-    public ArrayList<ManageDto> findProduct(String pcode) {
-    	List<ManageDto> resultList = sqlSession.selectList(namespace + ".findProduct",pcode);
-		ArrayList<ManageDto> manageList =new ArrayList<>(resultList);
+	}
+
+	@Override
+	public ArrayList<ManageDto> findProductManage(String pcode) {
+		List<ManageDto> resultList = sqlSession.selectList(namespace + ".findProductManage", pcode);
+		ArrayList<ManageDto> manageList = new ArrayList<>(resultList);
 		return manageList;
-    }
+	}
 
-    @Override
-    public int insertProduct(String adminid, int stock, int price) {
-        return sqlSession.insert(namespace + ".insertProduct");
-    }
+	@Override
+	public int insertProductManage(String adminid, int stock, int price) {
+		return sqlSession.insert(namespace + ".insertProductManage");
+	}
 
-    @Override
-    public int updateProduct(String adminid, String pcode, int stock, int price) {
-        return sqlSession.update(namespace + ".updateProduct");
-    }
+	@Override
+	public int updateProductManage(String adminid, String pcode, int stock, int price) {
+		return sqlSession.update(namespace + ".updateProductManage");
+	}
 
-    @Override
-    public int deleteProduct(String pcode) {
-        return sqlSession.update(namespace + ".deleteProduct");
-    }
+	@Override
+	public int deleteProductManage(String pcode) {
+		return sqlSession.update(namespace + ".deleteProductManage");
+	}
 
-    @Override
-    public int recoverProduct(String pcode) {
-        return sqlSession.update(namespace + ".recoverProduct");
-    }
+	@Override
+	public int recoverProductManage(String pcode) {
+		return sqlSession.update(namespace + ".recoverProductManage");
+	}
 
 }
